f4c83c61c394dc89762118e413b478c72adee951
Ayush Tripathi <yush.pbh@gmail.com>
2024-08-18 09:27:39 +0530

    Added Required Packages, and Connected User Routes

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 import express from "express";
+import bodyParser from "body-parser";
 import d from "dotenv";
-import db from "./db/db_connect.js";
-
+import userRouter from "./routes/user.js";
 d.config();
 
-const app = express();
-
+// Constants & Initialization
 const PORT = process.env.PORT | 3000;
 
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Routes
+app.use("/api/user", userRouter);
 app.get("/", (req, res) => {
     return res.status(200).json({ message: "Hello" });
 });
+
+// Server Listen
 
 app.listen(PORT, () => {
     console.log(`Server Started @ http://127.0.0.1:${PORT}`);
